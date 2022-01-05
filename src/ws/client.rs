869223e395b54640d87e50c1025b21664a61b8b7
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-01-05 08:36:04 -0500

    Rename `Ws` to `Gateway`

--- src/ws/client.rs
+++ src/ws/client.rs
@@ -1,19 +1,18 @@
 use super::WsStream;
 
 use tokio_tungstenite::{connect_async_with_config, tungstenite::protocol::{Message, WebSocketConfig}};
-
 use url::Url;
 
 use crate::internal::prelude::*;
 
-pub struct Ws {
+pub struct Gateway {
     pub stream: WsStream,
     pub url: Url,
     pub token: String,
 }
 
-impl Ws {
-    pub async fn new(url: &str, token: String) -> Result<Ws> {
+impl Gateway {
+    pub async fn new(url: &str, token: String) -> Result<Self> {
         let url = Url::parse(url).unwrap_or_else(|e| panic!("Failed to parse url: {}", e));
 
         let stream = connect_async_with_config(url, WebSocketConfig {
