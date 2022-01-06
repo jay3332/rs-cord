61ef480b36508bac654cf612a69db9c345f82a5c
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-01-05 18:03:32 -0800

    More ws impl

--- src/ws/client.rs
+++ src/ws/client.rs
@@ -1,8 +1,16 @@
 use super::WsStream;
 
+use std::env::consts;
+
 use tokio_tungstenite::{connect_async_with_config, tungstenite::protocol::{Message, WebSocketConfig}};
 
 use url::Url;
+
+use flate2::read::ZlibDecoder;
+
+use futures_util::{SinkExt, StreamExt};
+
+use serde_json::{json, Value};
 
 use crate::internal::prelude::*;
 
@@ -24,4 +32,57 @@ impl Ws {
 
         Ok(Ws { stream, url, token })
     }
+
+    pub async fn connect(reconnect: bool) -> Result<()> {
+        Ok(()) // TODO
+    }
+
+    pub async fn recv_json(&mut self) -> Result<Option<Value>> {
+        handle_ws_message(self.stream.next().await)
+    }
+
+    pub async fn send_json(&mut self, payload: &Value) -> Result<()> {
+        Ok(serde_json::to_string(payload)
+            .map(Message::Text)
+            .map_err(Error::from)
+            .map(|m| self.stream.send(m))?
+            .await?)
+    }
+
+    async fn identify(&mut self) -> Result<()> {
+        Ok(self.send_json(
+            &json!({
+                "op": 2_u8,
+                "d": {
+                    "token": self.token,
+                    "intents": self.intents.value,
+                    "compress": true,
+                    "large_threshold": 250_u8,
+                    // shard
+                    "presence": self.presence,
+                    "properties": {
+                        "$os": consts::OS,
+                        "$browser": "rs-cord",
+                        "$device": "rs-cord",
+                    }
+                }
+            })
+        ).await?)
+    }
+]
+
+#[inline]
+pub fn handle_ws_message(message: Option<Message>) -> Result<Option<Value>> {
+    Ok(
+        match message {
+            Some(Message::Binary(bytes)) => {
+                serde_json::from_reader(ZlibDecoder::new(&bytes[..])).map(Some).map_err(Error::from)?
+            },
+            Some(Message::Text(text)) => {
+                serde_json::from_str(&text).map(Some).map_err(Error::from)?
+            },
+            Some(Message::Close(Some(frame))) => None, // TODO: handle close
+            _ => None,
+        }
+    )
 }
