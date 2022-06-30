4caa5cb99b9abbf19d488d0b543e6f5b064857d7
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-06-29 20:37:08 -0400

    Changes by alvin
    Co-authored-by: Cryptex-github <64497526+Cryptex-github@users.noreply.github.com>

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,17 @@
 use thiserror::Error as _Error;
 use tokio_tungstenite::tungstenite::Error as TungsteniteError;
 use reqwest::Error as ReqwestError;
+
+#[derive(Debug, _Error)]
+pub enum Error {
+    #[error("WebSocket error: `{0}`")]
+    WebSocket(#[from] TungsteniteError),
+
+    #[error("HTTP Error: `{0}`")]
+    Http(#[from] ReqwestError),
+
+    #[error("rs-cord error: `{0}`")]
+    Library(String),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
